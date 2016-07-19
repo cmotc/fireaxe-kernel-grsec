2e4bf4a3a5192ae7561717e66d4488097c98a851
idk <idk@devuan.local>
2016-07-19 19:49:15 -0400

    updated to kernel 4.6 and moved build tree up

--- README.md
+++
@@ -1,2 +0,0 @@
-# fireaxe-kernel-grsec-4.4
-Custom grsecurity-ready kernel for debian. Don't use it unless you review it! I'm not an expert, as far as I know so far.
---
+++ tools/lib/lockdep/common.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <linux/compiler.h>
+#include <linux/lockdep.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+
+static __thread struct task_struct current_obj;
+
+/* lockdep wants these */
+bool debug_locks = true;
+bool debug_locks_silent;
+u32 prandom_u32(void)
+{
+	/* Used only by lock_pin_lock() which is dead code */
+	abort();
+}
+
+__attribute__((destructor)) static void liblockdep_exit(void)
+{
+	debug_check_no_locks_held();
+}
+
+struct task_struct *__curr(void)
+{
+	if (current_obj.pid == 0) {
+		/* Makes lockdep output pretty */
+		prctl(PR_GET_NAME, current_obj.comm);
+		current_obj.pid = syscall(__NR_gettid);
+	}
+
+	return &current_obj;
+}
