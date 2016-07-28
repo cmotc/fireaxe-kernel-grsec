1fb80d936793fbd513482addc923806305d88acb
idk <idk@devuan.local>
2016-07-27 20:17:57 -0400

    updated build script

--- debian.sh
+++ debian.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
+
+if [ -n "$BASH_VERSION" ]; then
+	TOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+else
+	TOME=$( cd "$( dirname "$0" )" && pwd )
+fi
+
+cd $TOME
+
 cp arch/arm/configs/antmseven_config .config
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
 make deb-pkg
 #make mrproper
