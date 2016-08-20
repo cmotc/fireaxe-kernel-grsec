474a2d8931de9239756274dcf703ee34c737243a
idk <idk@devuan.local>
2016-08-20 04:27:55 -0400

    added static config

--- debian.sh
+++ debian.sh
@@ -10,7 +10,8 @@ cd $TOME
 
 #cp arch/arm/configs/antmseven_config .config
 #cp arch/arm/configs/sunxi_fb_defconfig .config
-cp arch/arm/configs/antm_config_experimental .config
+cp arch/arm/configs/antm_seven_static .config
+export LDFLAGS='-static'
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 make olddefconfig
