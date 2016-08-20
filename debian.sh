848ddc0e6645f0dd0176c9f95c49ba541591697c
idk <idk@devuan.local>
2016-08-20 05:01:30 -0400

    build all 3 at once

--- debian.sh
+++ debian.sh
@@ -10,12 +10,21 @@ cd $TOME
 
 #cp arch/arm/configs/antmseven_config .config
 #cp arch/arm/configs/sunxi_fb_defconfig .config
+#BUILDS="arch/arm/configs/antmseven_config arch/arm/configs/sunxi_fb_defconfig arch/arm/configs/antm_seven_static"
+#for config in $BUILDS; do
+#        make mrproper
+#        cp arch/arm/configs/antm_seven_static .config
+#        export LDFLAGS='-static'
+#        export ARCH=arm
+#        export CROSS_COMPILE=arm-linux-gnueabihf-
+#        make olddefconfig
+#        make dtbs
+#        make -j4 deb-pkg
+#done
 cp arch/arm/configs/antm_seven_static .config
 export LDFLAGS='-static'
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 make olddefconfig
 make dtbs
-#fakeroot debian/rules binary
 make -j4 deb-pkg
-#make mrproper
