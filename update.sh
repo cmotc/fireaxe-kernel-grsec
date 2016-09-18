ea31c41fe65539b264c201881d495218138e5962
idk <idk@devuan.local>
2016-09-18 11:19:20 -0400

    added an updater script

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 tar xvfJ /usr/src/linux-grsec-source-4.7.tar.xz -C ./.tmp
 cp -Rfv .tmp/*/* $HOME/Projects/nightly-env/fireaxe-kernel-grsec/
-tar xvfJ /usr/src/linux-patch-4.7-grsec.patch.xz -C ./.tmp
+xz -dk /usr/src/linux-patch-4.7-grsec.patch.xz ./.tmp/linux-patch-4.7-grsec.patch
+cp ./.tmp/linux-patch-4.7-grsec.patch $HOME/Projects/nightly-env/
+patch -p1 < ../linux-patch-4.7-grsec.patch
+\rm -rf .tmp
 
